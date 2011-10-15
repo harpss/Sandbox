3436a756571c70afa774872f141c04b8fa31e07b
Harp Nagra <harpss@hotmail.com>
2011-10-15 01:18:04 -0700

    appending scores to a list and printing out the first three scores

--- Python/high_score.py
+++ Python/high_score.py
@@ -1,12 +1,12 @@
-highest_score = 0
+scores = []
+
 result_f = open("results.txt")
 for line in result_f:
-    (name, score) = line.split()
-    if float(score) > highest_score:
-        highest_score = float(score)
-        highest_scorer = name
+    scores.append(line)
 result_f.close()
-print("The highest score was: ")
-print(highest_score)
-print(" , scored by ")
-print(name)
+
+print (scores[0])
+print (scores[1])
+print (scores[2])
+
+    
