79b02fd7574f3e4f31d09abddf9f415f1ebe0b57
Harp Nagra <harpss@hotmail.com>
2011-10-15 02:57:12 -0700

    Added a sort to the hash
    The sort is applied to the hash based on the key which is the score.

--- Python/scores_hash.py
+++ Python/scores_hash.py
@@ -7,5 +7,5 @@ for line in result_f:
 result_f.close()
 
 print ("The top scores were:")
-for each_score in scores.keys():
+for each_score in sorted(scores.keys(), reverse = True):
     print('Surfer ' + scores[each_score] + ' scored ' + each_score)
