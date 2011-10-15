8803e4ed0510eee22e346ae87f31f226e513a9a8
Harp Nagra <harpss@hotmail.com>
2011-10-15 02:15:37 -0700

    sorting with the name using 2 arrays
    this doesn't work properly since the 2 arrays are sorted independently
    and the names and scores are no longer connected.

--- Python/high_score.py
+++ Python/high_score.py
@@ -1,18 +1,20 @@
 scores = []
+names = []
 
 result_f = open("results.txt")
 for line in result_f:
     (name, score) = line.split()
     scores.append(float(score))
+    names.append(name)
 result_f.close()
 
-scores.sort()
-scores.reverse()
+scores.sort(reverse = True)
+names.sort(reverse = True)
 
 print ("the top scores were :")
 
-print (scores[0])
-print (scores[1])
-print (scores[2])
+print (names[0] + ' with ' + str(scores[0]))
+print (names[1] + ' with ' + str(scores[1]))
+print (names[2] + ' with ' + str(scores[2]))
 
     
